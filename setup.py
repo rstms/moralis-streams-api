9975ca4385f894d0e9f1eeaa45d3b11bfd23595a
Matt Krueger <mkrueger@rstms.net>
2022-10-05 03:06:31 -0500

    change name in setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 
 from setuptools import setup, find_packages  # noqa: H301
 
-NAME = "moralis-streams-api-client"
+NAME = "moralis_streams_api"
 VERSION = "1.0.0"
 # To install the library, run the following
 #
